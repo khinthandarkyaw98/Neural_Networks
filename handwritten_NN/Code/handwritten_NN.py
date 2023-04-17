492c790fde394e24b1999e714be93d1f819dda06
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-17 19:31:47 +0700

    added costfunc, bias and initialized weights

--- handwritten_NN/Code/handwritten_NN.py
+++ handwritten_NN/Code/handwritten_NN.py
@@ -106,5 +106,44 @@ there cost function comes in
 from which the model learns its mistakes in terms of backward propagation
 """
 
+"""
+This is a cost function for binary classification 
+    J(y, y_hat) = -1/m * sum(y * log(y_hat) + (1-y) * log(1-y_hat))
+where y is the true binary label (0 or 1), 
+y_hat is the predicted probability (between 0 and 1), and 
+m is the number of samples.
+"""
 
+def cal_cost(y_enc, output): # y_enc = ground_truth, output = output from the model
+    t1 = -y_enc * np.log(output)
+    t2 = (1-y_enc) * np.log(1-output)
+    cost = np.sum(t1 - t2) # we have minus before the sum in the formula, so let's take minus in the code eq, so it will be the plus, minus means penality, so we will have penalities for 0 and penalities for 1
+    return cost
+
+def add_bias_unit(X, where):
+    # where is just row or column
+    if where == 'column':
+        X_new = np.ones((X.shape[0], X.shape[1] + 1)) # same size but with an extra column
+        X_new[:, 1:] = X # from col 1 to len(col) : our old array, so col 0 is just an a column all filled with ones
+    if where == 'row':
+        X_new = np.ones((X.shape[0] + 1, X.shape[1]))
+        X_new[1:, :] = X # row 1 to len(row): our old array, so row 0 is just an a row all filled with ones
+    return X_new
+
+"""
+    n_features = the initial input which is 28 x 28, flattened into column vector = 784
+    n_output = 10 bc of handwritten number images from 0 to 9 for classification
     
+"""
+def init_weights(n_features, n_hidden, n_output):
+    # initially, weights are random numbers between -1 and 1
+    w1 = np.random.uniform(-1.0, 1.0, size = n_hidden * (n_features + 1))
+    # it comes out in the wrong dimensionality, so we need to reshape it
+    w1 = w1.reshape(n_hidden, n_features + 1)
+    w2 = np.random.uniform(-1.0, 1.0, size = n_hidden * (n_hidden + 1))
+    w2 = w2.reshpae(n_hidden, n_hidden + 1)
+    w3 = np.random.uniform(-1.0, 1.0, size = n_output * (n_hidden + 1))
+    w3 = w3.reshape(n_output, n_hidden +1)
+    return w1, w2, w3
+    
+
