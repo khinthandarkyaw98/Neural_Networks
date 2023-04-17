f175402d7ef115da05046d1afe15326088aced45
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-17 14:31:03 +0700

    added git doc

---
+++ handwritten_NN/Code/git.py
@@ -0,0 +1,8 @@
+    """_echo "# Neural_Networks" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/khinthandarkyaw98/Neural_Networks.git
+git push -u origin main
+    """
