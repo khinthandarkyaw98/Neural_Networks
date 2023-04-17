875e20bb5876f3498034ba2584df6f9acb465bb0
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-17 14:59:45 +0700

    modified and deleted

--- handwritten_NN/Code/git.py
+++ handwritten_NN/Code/git.py
@@ -6,3 +6,16 @@ git branch -M main
 git remote add origin https://github.com/khinthandarkyaw98/Neural_Networks.git
 git push -u origin main
     """
+    
+    # for test branch
+    
+    """git checkout -b test    # creates a new branch named "test" and switches to it
+...                     # make changes to your code in the "test" branch
+git add .               # add any changes to the staging area
+git commit -m "commit message"   # commit your changes
+git push origin test    # push your changes to the remote "test" branch
+git checkout main       # switch back to the "main" branch
+git merge test          # merge the changes from the "test" branch into the "main" branch
+git push origin main    # push the merged changes to the remote "main" branch
+
+    """
