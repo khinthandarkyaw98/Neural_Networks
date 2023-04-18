6cb6fd24773a7978762a8bc863c00d4beea3d3b9
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-18 17:49:37 +0700

    finished

--- handwritten_NN/Code/handwritten_NN.py
+++ handwritten_NN/Code/handwritten_NN.py
@@ -149,6 +149,12 @@ def init_weights(n_features, n_hidden, n_output):
 
 """
 x = input
+you can take a look at nn_visualize.ipynb for more intuition
+we will use bias in the input before we multiply by weights
+after the mulipliacation of input with bias by weights
+we will pass it into the activation function for 'repress' or 'amplify'
+then we got the new hidden layer
+repeat this process
 """
 def feed_forward(x, w1, w2, w3):
     # add bias unit to the input
@@ -166,6 +172,112 @@ def feed_forward(x, w1, w2, w3):
     z4 = w3.dot(a3)
     a4 = sigmoid(z4)
     
-    return a1, z2, a2, z3, a3, z4, z4 
+    return a1, z2, a2, z3, a3, z4, a4
 
+def predict(x, w1, w2, w3):
+    a1, z2, a2, z3, a3, z4, a4 = feed_forward(x, w1, w2, w3)
+    y_pred = np.argmax(a4, axis = 0) # this will give the index number at which the maximum probability exists
+    return y_pred
 
+"""
+take the outputs and weights
+we do some backward propagation
+
+As you can see, the very first step, 
+delta4 is calculating the error 
+between the predicted value and the ground truth ones.
+Follwoing this, we will add bias to the previous layer output. 
+Then delta3 is the multiplication of 
+the dot product of current weights_3_transpose and the next delta_4 
+and the current_sigmoid_gradient_z3.
+"""
+def cal_grad(a1, a2, a3, a4, z2, z3, z4, y_enc, w1, w2, w3):
+    delta4 = a4 - y_enc
+    z3 = add_bias_unit(z3, where='row')
+    delta3 = w3.T.dot(delta4) * sigmoid_gradient(z3)
+    # discard the first row
+    # bc we do not care about the values of the bias unit 
+    # which located at the very first row as we put where='row'
+    delta3 = delta3[1:, :]
+    z2 = add_bias_unit(z2, where='row')
+    delta2 = w2.T.dot(delta3) * sigmoid_gradient(z2)
+    delta2 = delta2[1:, :]
+    
+    grad1 = delta2.dot(a1)
+    grad2 = delta3.dot(a2.T)
+    grad3 = delta4.dot(a3.T)
+    
+    return grad1, grad2, grad3
+    
+def runModel(X, y, X_t, y_t):
+    X_copy, y_copy = X.copy(), y.copy()
+    y_enc = enc_one_hot(y)
+    epochs = 10
+    batch = 5
+    
+    w1, w2, w3 = init_weights(784, 75, 10)
+    
+    alpha = 0.001
+    eta = 0.001
+    dec = 0.00001
+    delta_w1_prev = np.zeros(w1.shape)
+    delta_w2_prev = np.zeros(w2.shape)
+    delta_w3_prev = np.zeros(w3.shape)
+    total_cost = []
+    pred_acc = np.zeros(epochs)
+    
+    for i in range(epochs):
+        
+        shuffle = np.random.permutation(y_copy.shape[0])
+        X_copy, y_enc = X_copy[shuffle], y_enc[:, shuffle]
+        eta /= (1 + dec*i)
+        
+        mini = np.array_split(range(y_copy.shape[0]), batch)
+        
+        for step in mini:
+            a1, z2, a2, z3, a3, z4, a4 = feed_forward(X_copy[step], w1, w2, w3)
+            cost = cal_cost(y_enc[:, step], a4)
+            
+            total_cost.append(cost)
+            # back propagate
+            grad1, grad2, grad3 = cal_grad(a1, a2, a3, a4, z2, z3, z4, y_enc[:, step], w1, w2, w3)
+            delta_w1, delta_w2, delta_w3 = eta * grad1, eta * grad2, eta * grad3
+            
+            w1 -= delta_w1 + alpha * delta_w1_prev
+            w2 -= delta_w2 + alpha * delta_w2_prev
+            w3 -= delta_w3 + alpha * delta_w3_prev
+            
+            delta_w1_prev, delta_w2_prev, delta_w3_prev = delta_w1, delta_w2, delta_w3_prev
+
+        y_pred = predict(X_t, w1, w2, w3)
+        pred_acc[i] = 100 * np.sum(y_t == y_pred, axis = 0) / X_t.shape[0]
+        print('epoch #', i)
+    return total_cost, pred_acc, y_pred
+
+train_x, train_y, test_x, test_y = load_data()
+
+cost, acc, y_pred = runModel(train_x, train_y, test_x, test_y)
+
+x_a = [i for i in range(acc.shape[0])]
+x_c = [i for i in range(len(cost))]
+print('final prediction accuracy is: ', acc[9])
+plt.subplot(221)
+plt.plot(x_c, cost)
+plt.subplot(222)
+plt.plot(x_a, acc)
+plt.show()
+
+miscl_img = test_x[test_y != y_pred][:25]
+correct_lab = test_y[test_y != y_pred][:25]
+miscl_lab = y_pred[test_y != y_pred][:25]
+
+fig, ax = plt.subplots(nrows = 5, ncols = 5, sharex = True, sharey = True)
+ax = ax.flatten()
+for i in range(25):
+    img = miscl_img[i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+    ax[i].set_title('%d) t: %d p: % d' % (i+1, correct_lab[i], miscl_lab[i]))
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
