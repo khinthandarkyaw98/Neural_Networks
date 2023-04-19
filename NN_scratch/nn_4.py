e76ac7dda0510d8e2692d44f00047990a57e4739
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-19 15:47:32 +0700

    modified

---
+++ NN_scratch/nn_4.py
@@ -0,0 +1,13 @@
+
+"""
+Dot product
+
+"""
+import numpy as np
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
+
+output = np.dot(inputs, weights) + bias
+print(f"output = {output}")
