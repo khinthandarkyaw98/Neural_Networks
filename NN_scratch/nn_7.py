ac53d028562a697accbee557f37d5e631c209e62
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-19 18:18:49 +0700

    added 2 hidden layers

---
+++ NN_scratch/nn_7.py
@@ -0,0 +1,31 @@
+"""
+    Add layers
+"""
+import numpy as np
+
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0], 
+          [-1.5, 2.7, 3.3, -0.8]]
+
+# Hidden Layer 1
+
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+
+# Hidden Layer 2
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+# output of Hidden Layer 1
+layer1_output = np.dot(inputs, np.array(weights).T) + biases
+# output of Hidden Layer 2
+layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
+
+print(f"layer2_output = {layer2_output}")
