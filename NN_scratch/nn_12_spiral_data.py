04adb60511babc9bf4682347084eb7d4c0fe67f1
khinthandarkyaw98 <khinthanderkyaw201493@gmail.com>
2023-04-20 20:18:09 +0700

    fixed spiral and added nn_15

--- NN_scratch/nn_12_spiral_data.py
+++ NN_scratch/nn_12_spiral_data.py
@@ -15,7 +15,7 @@ def create_data(points, classes):
         ix = range(points * class_number, points * (class_number + 1))
         r = np.linspace(0.0, 1, points) # radius
         t = np.linspace(class_number * 4, (class_number + 1) * 4, points) + np.random.randn(points) * 0.2
-        # np.c_ : concatenate along axis = 1
+        # np.c_ : concatenate along axis = 1 -> same row : diff col
         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
         y[ix] = class_number
     return X, y
---
+++ NN_scratch/nn_15_batch_norm0.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# use pip install nnfs to keep version same as that of nnfs book
+import nnfs 
+
+#np.random.seed(0)
+# we do not need seed now
+# let's use nnfs
+nnfs.init()
+
+# outputs in batches
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
+
+exp_values = np.exp(layer_outputs)
+print(f"exp_values = {exp_values}")
+
+norm_base_test1 = np.sum(layer_outputs)
+print(f"norm_base_test1 = {norm_base_test1}")
+"""
+norm_base_test1 = 18.172
+"""
+
+
+
+norm_base_test2 = np.sum(layer_outputs, axis = 1)
+print(f"norm_base_test2 = {norm_base_test2}")
+"""
+norm_base_test2 = [8.395 7.29  2.487]
+"""
+
+norm_base_test3 = np.sum(layer_outputs, axis=1, keepdims=True)
+print(f"norm_base_test3 = {norm_base_test3}")
+"""
+norm_base_test3 = [[8.395]
+            [7.29 ]
+            [2.487]]
+"""
+
+"""
+Now, we now how to calculate norm_base by same row diff cols.
+"""
+norm_base = np.sum(exp_values, axis=1, keepdims=True)
+norm_values = exp_values / norm_base
+print(f"norm_values = {norm_values}")
+
+"""
+norm_values = [[8.95282664e-01 2.47083068e-02 8.00090293e-02]
+                [9.99811129e-01 2.23163963e-05 1.66554348e-04]
+                [5.13097164e-01 3.58333899e-01 1.28568936e-01]]
+"""
